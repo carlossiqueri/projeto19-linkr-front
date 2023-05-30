a3f702d17dbf7b37cecebca2f984f64ae23b9dce
Fabio Caprara <fats140701@gmail.com>
2023-05-30 15:51:53 -0300

    feat: signup

---
+++ src/components/SignupPage/SignupPage.js
@@ -0,0 +1,12 @@
+import AuthPage from "../AuthPage/AuthPage";
+import SignupForm from "../SignupForm/SignupForm";
+
+const SignupPage = () => {
+  return (
+    <AuthPage>
+      <SignupForm />
+    </AuthPage>
+  );
+};
+
+export default SignupPage;
