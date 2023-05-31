4ccb84de5852f4d0ea83bbf5c523717005ca3e90
Fabio Caprara <fats140701@gmail.com>
2023-05-31 01:46:04 -0300

    feat: logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,51 +1,119 @@
+import React from "react";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
-export default function Header(){
-    return(
-        <HeaderContainer>
-            <p>linkr</p>
-            <span> <ion-icon name="chevron-down-outline"></ion-icon> <img /></span>
+export default function Header({ setIsAuthenticated, setSession }) {
+  const navigate = useNavigate();
+  const [isOpenMenu, setIsOpenMenu] = React.useState(false);
+  const typeIonIcon = isOpenMenu
+    ? "chevron-up-outline"
+    : "chevron-down-outline";
+  const menuRef = React.useRef();
+  const imgRef = React.useRef();
 
-        </HeaderContainer>
-    )
+  function toggleLogoutMenu(e) {
+    setIsOpenMenu((prevState) => !prevState);
+  }
+
+  React.useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (imgRef.current.contains(event.target)) {
+        return;
+      }
+      if (!menuRef.current.contains(event.target)) {
+        setIsOpenMenu(false);
+      }
+    };
+
+    document.addEventListener("click", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, []);
+
+  function handleLogout() {
+    setIsAuthenticated(false);
+    setSession(null);
+    localStorage.removeItem("token");
+    navigate("/");
+  }
+
+  return (
+    <HeaderContainer>
+      <p>linkr</p>
+      <span ref={imgRef} onClick={toggleLogoutMenu}>
+        <ion-icon name={typeIonIcon}></ion-icon> <img />
+      </span>
+
+      <div
+        onClick={handleLogout}
+        ref={menuRef}
+        style={{ display: isOpenMenu ? "initial" : "none" }}
+      >
+        <span>Logout</span>
+      </div>
+    </HeaderContainer>
+  );
 }
 
 const HeaderContainer = styled.section`
-background-color: #151515;
-display: flex;
-justify-content: space-between;
-align-items: center;
-width: 100%;
-height: 72px;
-color: #FFFFFF;
+  background-color: #151515;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 72px;
+  color: #ffffff;
 
-position: fixed;
-top: 0;
-left: 0;
-z-index: 1;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1;
 
-p{
-    font-family: 'Passion One';
+  p {
+    font-family: "Passion One";
     font-style: normal;
     font-weight: 700;
     font-size: 49px;
     line-height: 54px;
     letter-spacing: 0.05em;
     margin-left: 28px;
-}
+  }
 
-span{
+  span {
     font-size: 25px;
     display: flex;
     align-items: center;
+    cursor: pointer;
 
-    img{
-        width: 53px;
-        height: 53px;
-        border-radius: 26.5px;
-        background-color: purple;
-        margin-right: 17px;
-        margin-left: 16.3px;
+    img {
+      width: 53px;
+      height: 53px;
+      border-radius: 26.5px;
+      background-color: purple;
+      margin-right: 17px;
+      margin-left: 16.3px;
     }
-}
-`
+  }
+
+  > div {
+    position: fixed;
+    top: 72px;
+    right: 0;
+    background-color: #151515;
+    padding: 25px;
+    border-radius: 0 0 0 20px;
+    font-family: "Lato";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 17px;
+    line-height: 20px;
+    letter-spacing: 0.05em;
+    color: #ffffff;
+  }
+
+  > div > span {
+    cursor: pointer;
+  }
+`;
