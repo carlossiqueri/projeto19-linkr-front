e87f0dc593210d217e621d3755949fcd0f8b9be7
Lorena <blorena1321@gmail.com>
2023-06-05 13:57:24 -0300

    salvando funcionalidades testes para acrescentar na main

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,11 +3,16 @@ import Header from "../components/Header";
 import { useContext, useState } from "react";
 import axios from "axios";
 import { InfoContext } from "../context/InfoContext";
+import Modal from "react-modal";
+import ReactModal from "react-modal";
+import { ColorRing } from "react-loader-spinner";
 
 export default function Homepage({ setIsAuthenticated, setSession }) {
   const [form, setForm] = useState({ url: "", description: "" });
   const [disabled, setDisabled] = useState(false);
   const { token } = useContext(InfoContext);
+  const [openedModal, setOpenedModal] = useState(false);
+  const [delected, setDelected] = useState(false);
 
   function handleForm(e) {
     setForm({ ...form, [e.target.name]: e.target.value });
@@ -21,15 +26,15 @@ export default function Homepage({ setIsAuthenticated, setSession }) {
     setTimeout(() => {
       const urlPost = `${process.env.REACT_APP_API_URL}newPost`;
       const body = { url: form.url, description: form.description };
-      const config= {
-          headers:{
-              Authorization: `Bearer ${token}`
-          }
-      }
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
       const promise = axios.post(urlPost, body, config);
       promise.then((res) => {
         console.log("Deu certo!!!");
-        setForm({url: "", description: ""});
+        setForm({ url: "", description: "" });
       });
       promise.catch((err) => {
         console.log(err.response.data);
@@ -42,10 +47,41 @@ export default function Homepage({ setIsAuthenticated, setSession }) {
     }, 3000);
   }
 
+  function openModal() {
+    setOpenedModal(true);
+  }
 
-    return(
-        <HomepageContainer>
-            <Header setIsAuthenticated={setIsAuthenticated} setSession={setSession} />
+  function deletePost(id){
+    setDelected(true);
+
+    setTimeout(() => {
+      const urlDelete= `${process.env.REACT_APP_API_URL}`;
+    const config = {
+      headers:
+      {Authorization: `Bearer ${token}`}
+    }
+
+    const promise = axios.delete(urlDelete, config);
+    promise.then(res => {
+      setDelected(false);
+      setOpenedModal(false);
+    });
+    promise.catch((err) => {
+      console.log(err.response.data.mensagem);
+      setOpenedModal(false);
+      alert("Não foi possível excluir o post");
+    });
+    promise.finally(() => {
+      setOpenedModal(false);
+    })
+    
+    }, 3000);
+    
+  }
+
+  return (
+    <HomepageContainer>
+      <Header setIsAuthenticated={setIsAuthenticated} setSession={setSession} />
 
       <FeedContainer disabled={disabled}>
         <p>timeline</p>
@@ -56,39 +92,67 @@ export default function Homepage({ setIsAuthenticated, setSession }) {
             <p>What are you going to share today?</p>
 
             <form onSubmit={createPost}>
-                    <input
-                    required
-                    placeholder="http://..."
-                    type="text"
-                    name="url"
-                    value={form.url}
-                    onChange={handleForm}
-                    disabled={disabled}
-                     />
+              <input
+                required
+                placeholder="http://..."
+                type="text"
+                name="url"
+                value={form.url}
+                onChange={handleForm}
+                disabled={disabled}
+              />
 
-                    <textarea
-                    required
-                    placeholder="Awesome article about #javascript"
-                    type="text"
-                    name="description"
-                    value={form.description}
-                    onChange={handleForm}
-                    disabled={disabled}
-                     />
+              <textarea
+                required
+                placeholder="Awesome article about #javascript"
+                type="text"
+                name="description"
+                value={form.description}
+                onChange={handleForm}
+                disabled={disabled}
+              />
 
-{disabled ? (
+              {disabled ? (
                 <button disabled={disabled}> Publishing... </button>
               ) : (
                 <button> Publish </button>
               )}
             </form>
 
-                    </div>
-                </span>
-            </FeedContainer>
+            <button onClick={openModal}>Teste</button>
+          </div>
+        </span>
+      </FeedContainer>
 
-        </HomepageContainer>
-    )
+      <StyledModal isOpen={openedModal} style={customStyles}>
+        <p>
+          Are you sure you want <br /> to delete this post?
+        </p>
+
+
+          {delected ? (
+           <ColorRing
+           visible={true}
+           height="80"
+           width="80"
+           ariaLabel="blocks-loading"
+           wrapperStyle={{}}
+           wrapperClass="blocks-wrapper"
+           colors={["#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF" ]}
+         />
+            
+          ) : (
+            <div>
+            <WhiteButton onClick={() => setOpenedModal(false)}>No, go back</WhiteButton>
+            <BlueButton onClick={deletePost}>Yes, delete it</BlueButton>
+            </div>
+          )}
+        
+        
+        
+      </StyledModal>
+    </HomepageContainer>
+  );
 }
 
 const HomepageContainer = styled.section`
@@ -134,7 +198,6 @@ const FeedContainer = styled.div`
       display: flex;
       flex-direction: column;
       width: 502px;
-      
 
       p {
         font-family: "Lato", sans-serif;
@@ -149,39 +212,39 @@ const FeedContainer = styled.div`
         display: flex;
         flex-direction: column;
 
-    input {
-        background-color: #EFEFEF;
-        border-radius: 5px;
-        border: none;
-        width: 100%;
-        padding: 10px 0 10px 10px;
-        outline: none;
-        margin-bottom: 5px;
+        input {
+          background-color: #efefef;
+          border-radius: 5px;
+          border: none;
+          width: 100%;
+          padding: 10px 0 10px 10px;
+          outline: none;
+          margin-bottom: 5px;
 
-        font-family: 'Lato', sans-serif;
-        font-size: 15px;
-        font-weight: 300;
-        line-height: 18px;
-        color: ${({disabled}) => disabled  ? "#949494" : "#000000"};
-    }
+          font-family: "Lato", sans-serif;
+          font-size: 15px;
+          font-weight: 300;
+          line-height: 18px;
+          color: ${({ disabled }) => (disabled ? "#949494" : "#000000")};
+        }
 
-    textarea {
-        height: 66px;
-        padding: 10px 10px;
-        border: none;
-        border-radius: 5px;
-        background-color: #EFEFEF;
-        outline: none;
-        margin-bottom: 8px;
-        resize: none;
-        width: 100%;
+        textarea {
+          height: 66px;
+          padding: 10px 10px;
+          border: none;
+          border-radius: 5px;
+          background-color: #efefef;
+          outline: none;
+          margin-bottom: 8px;
+          resize: none;
+          width: 100%;
 
-        font-family: 'Lato', sans-serif;
-        font-size: 15px;
-        font-weight: 300;
-        line-height: 18px;
-        color: ${({disabled}) => disabled ? "#949494" : "#000000"};
-    }
+          font-family: "Lato", sans-serif;
+          font-size: 15px;
+          font-weight: 300;
+          line-height: 18px;
+          color: ${({ disabled }) => (disabled ? "#949494" : "#000000")};
+        }
       }
       button {
         background-color: #1877f2;
@@ -201,3 +264,70 @@ const FeedContainer = styled.div`
     }
   }
 `;
+
+const customStyles = {
+  
+  content: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "25%",
+    backgroundColor: "#333333",
+    margin: "auto",
+    borderRadius: "50px",
+    padding: "20px",
+
+    fontFamily: "Lato",
+    fontSize: "20px", 
+    fontWeight: "700",
+    lineHeight: "25.8px",
+    color: "#FFFFFF",
+    textAlign: "center"
+  },
+  overlay: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "rgba(255, 255, 255, 0.9)",
+    zIndex: "2"
+  },
+};
+
+const StyledModal = styled(ReactModal)`
+  ${customStyles.content}
+
+  div{
+    margin-top: 30px;
+    margin-bottom: 20px;
+  }
+`;
+
+const BlueButton = styled.button`
+background-color: #1877F2;
+font-family: "Lato", sans-serif;
+font-weight: 700;
+font-size: 14px;
+line-height: 16.8px;
+color: #ffffff;
+border: none;
+border-radius: 5px;
+cursor: pointer;
+padding: 5px;
+width: 112px;
+margin-left: 20px;
+`
+const WhiteButton = styled.button`
+background-color: white;
+font-family: "Lato", sans-serif;
+font-weight: 700;
+font-size: 14px;
+line-height: 16.8px;
+color: #1877F2;
+border: none;
+border-radius: 5px;
+cursor: pointer;
+padding: 5px;
+width: 112px;
+
+`
